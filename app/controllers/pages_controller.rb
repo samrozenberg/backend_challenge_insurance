d14466896dcab5dd2d9f021c4f0d8cf567bbf60e
Sam Rozenberg <sam.rozen@hotmail.com>
2022-12-14 13:05:38 +0100

    displaying simulations

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,17 +3,19 @@ class PagesController < ApplicationController
 
   def home
     # @quote = Quote.new
-    quote_params = {
-      "annualRevenue": params[:annual_revenue].to_i,
-      "enterpriseNumber": params[:enterprise_number],
-      "legalName": params[:legal_name],
-      "naturalPerson": params[:natural_person].to_i == 1,
-      "nacebelCodes": QuoteReader.new(params[:profession]).codes
-    }
-    @quote = ApiClient.new(quote_params).quote
+    if request.original_fullpath.include?("annual_revenue")
+      quote_params = {
+        "annualRevenue": params[:annual_revenue].to_i,
+        "enterpriseNumber": params[:enterprise_number],
+        "legalName": params[:legal_name],
+        "naturalPerson": params[:natural_person].to_i == 1,
+        "nacebelCodes": QuoteReader.new(params[:profession]).codes
+      }
+      @quote = ApiClient.new(quote_params).quote
 
-    # if @quote.dig("success")
-    #   Simulation.create(user: current_user, url: request.original_url)
-    # end
+      if @quote["success"] && Simulation.where(user: current_user, url: request.original_url).count.zero?
+        Simulation.create(user: current_user, url: request.original_url)
+      end
+    end
   end
 end
