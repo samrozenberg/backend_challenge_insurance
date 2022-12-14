0edbcef3f59ef57f8383ebf1990cc14d04c606c0
Sam Rozenberg <sam.rozen@hotmail.com>
2022-12-14 06:36:38 +0100

    structuring client action page

--- app/services/quote_reader.rb
+++ app/services/quote_reader.rb
@@ -3,11 +3,14 @@ require 'csv'
 class QuoteReader
   def initialize(query)
     @query = query
-
   end
 
   def codes
     return [] unless @query.present?
     CSV.read("NACEBEL_2008.csv", {col_sep: ";"}).select{|i| i[0].to_i == 5 && (i[3] + i[4] + i[5]).include?(@query)}.map{|i| i[1].to_s}
+  end
+
+  def countcodes
+    CSV.read("NACEBEL_2008.csv", {col_sep: ";"}).select{|i| i[0].to_i == 5}.count
   end
 end
